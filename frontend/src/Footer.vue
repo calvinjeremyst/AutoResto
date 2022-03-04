054217dfd4b41398734c8375bc48911392deca50
William Juniar <williamjuniar@gmail.com>
2022-03-04 21:43:37 +0700

    membuat Footer.vue

---
+++ frontend/src/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <p> {{ footer-text }} </p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                footerText: "Copyright by AutoResto"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    footer {
+        background-color: blanchedalmond;
+        padding: 10px;
+        text-align: center;
+    }
+</style>
