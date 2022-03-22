f0380bc7937a289dc6845b098802db33c1653b94
calvinjeremyst <calvinjeremyst@gmail.com>
2022-03-22 22:44:50 +0700

    Mengupdate Table

--- frontend/src/views/owner/RecipeList.vue
+++ frontend/src/views/owner/RecipeList.vue
@@ -9,6 +9,7 @@
           <th>Material</th>
           <th>Quantity</th>
           <th>Unit</th>
+          <th>Data Option</th>
         </tr>
         <tr v-for="log in data" :key="log.id">
           <td>{{ log.id }}</td>
@@ -16,6 +17,35 @@
           <td>{{ log.material }}</td>
           <td>{{ log.quantity }}</td>
           <td>{{ log.unit }}</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Nasi Goreng Spesial dengan Daging</td>
+          <td>Beras</td>
+          <td>200</td>
+          <td>Gr</td>
+          <td><button name="edit" class="btnUpdate">Edit</button><button name="delete" class="btnDelete">Delete</button></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Daging</td>
+          <td>100</td>
+          <td>Gram</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Cabai</td>
+          <td>2</td>
+          <td>Pcs</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Telur</td>
+          <td>2</td>
+          <td>Butir</td>
         </tr>
       </table>
     </center>
